6ddb2c2fbf057238129c7c34cb73adadc3493590
Jaiane <jaianesoares700@gmail.com>
2025-11-10 16:55:59 -0300

    feat:TrigésimoOitavo

---
+++ src/logica/Exercicio38.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Exercicio38 {
+    public static void main(String[] args) {
+
+        int contador = 6;
+
+        while (contador <= 11) {
+            System.out.print(contador + " ");
+            contador++;
+
+        }
+
+        System.out.println("Acabou!!");
+    }
+}
