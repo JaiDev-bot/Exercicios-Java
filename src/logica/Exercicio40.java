19d84cab6772e9b3a54bb7458960d6052f186576
Jaiane <jaianesoares700@gmail.com>
2025-11-10 17:17:46 -0300

    feat:Quadragesimo

---
+++ src/logica/Exercicio40.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Exercicio40 {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while(contador<=18 ){
+            System.out.print(contador  + " ");
+            contador +=3;
+
+        }
+
+        System.out.println("Acabou!!");
+
+    }
+}
