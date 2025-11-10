5b5857ae11093455ea8d76182ca9da1abb783c8e
Jaiane <jaianesoares700@gmail.com>
2025-11-10 17:00:48 -0300

    feat:TrigesimoNono

---
+++ src/logica/Exercicio39.java
@@ -0,0 +1,15 @@
+package logica;
+
+public class Exercicio39 {
+    public static void main(String[] args) {
+
+        int contador = 10;
+
+        while(contador>=3){
+            System.out.print(contador + " ");
+            contador --;
+        }
+
+        System.out.println("Acabou!!");
+    }
+}
