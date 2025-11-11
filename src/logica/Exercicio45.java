45772cccf9478f4a73da5b8a95cc13397d705ab2
Jaiane <jaianesoares700@gmail.com>
2025-11-11 15:42:36 -0300

    feat:QuadragesimoQuinto ex

---
+++ src/logica/Exercicio45.java
@@ -0,0 +1,49 @@
+package logica;
+
+import java.util.Scanner;
+
+public class Exercicio45 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+        System.out.println("Digite o numero inicial: ");
+        int inicio = scanner.nextInt();
+
+        System.out.println("Digite o numero final: ");
+        int numFinal = scanner.nextInt();
+
+        System.out.println("Digite o incremento: ");
+        int incremento = scanner.nextInt();
+
+
+        int contador = inicio;
+
+//            if (inicio>=numFinal){
+//                System.out.println("O numero inicial precisa ser menor que o numero final");
+//                continue;
+//            }
+
+        while (inicio <= numFinal) {
+
+            System.out.print(inicio + " ");
+            inicio += incremento;
+
+
+        }
+        while (contador>= numFinal) {
+            System.out.print(contador + " ");
+            contador -= incremento;
+
+        }
+
+        System.out.println("Acabou!!");
+
+        scanner.close();
+
+    }
+}
+
