9f31146d02b6e30ccae60e05562cab80b58d3feb
Jaiane <jaianesoares700@gmail.com>
2025-11-10 21:19:51 -0300

    feat:QuadragesimoTerceiro ex

---
+++ src/logica/Exercicio43.java
@@ -0,0 +1,19 @@
+package logica;
+
+public class Exercicio43 {
+    public static void main(String[] args) {
+    int contador = 30;
+
+    while (contador >=0){
+
+        if(contador % 4 ==0){
+            System.out.print("[" + contador + "]");
+        }else {
+
+            System.out.print(contador + " ");
+        }
+
+    }
+
+    }
+}
