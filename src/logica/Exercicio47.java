0f22359fe36c8ae6a1e52d4af1cf15a5f9d0809f
Jaiane <jaianesoares700@gmail.com>
2025-12-01 15:27:34 -0300

    feat: Quadragésimo sétimo ex

---
+++ src/logica/Exercicio47.java
@@ -0,0 +1,18 @@
+package logica;
+
+public class Exercicio47 {
+    public static void main(String[] args) {
+
+        int numInit = 500;
+        int numFinal = 0;
+
+
+        while ( numInit>=0){
+
+            System.out.print(numInit + " ");
+
+            numInit -= 50;
+        }
+
+    }
+}
